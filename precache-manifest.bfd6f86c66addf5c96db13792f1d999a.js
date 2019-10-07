b753af9a220b81b11f8fa48d055edc69116b393c
Kalysh <503292@ukr.net>
2019-10-07 09:31:14 +0300

    Updates

---
+++ precache-manifest.bfd6f86c66addf5c96db13792f1d999a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab19f308c578f33aa9becc589fcb4ebb",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "d2c26ccfbeab8e702614",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "9c0a3c36c4278845653f",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.22d6f411.chunk.css"
+  },
+  {
+    "revision": "d2c26ccfbeab8e702614",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.f3f2bd9b.chunk.js"
+  },
+  {
+    "revision": "9c0a3c36c4278845653f",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.80e9c8e4.chunk.js"
+  },
+  {
+    "revision": "0e8a307275672aae109e",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.625d1d36.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.../precache-manifest.d776c5fc7f0cd192ad6c4b88d43e3477.js"
+  "/goit-react-hw-02-bank-account/precache-manifest.bfd6f86c66addf5c96db13792f1d999a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.../index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-bank-account/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
